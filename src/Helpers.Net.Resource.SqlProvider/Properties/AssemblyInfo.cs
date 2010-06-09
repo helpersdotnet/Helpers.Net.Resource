7d9f69051d1abcb1b67e53a0181f9eb5fbcf1c53
Prabir Shrestha <prabir@softverk.is>
2010-06-09 17:34:06 +0700

    + ren Helpers.Net.Resoruce.Web to Helpers.Net.Resource.SqlProvider

--- src/Helpers.Net.Resource.SqlProvider/Properties/AssemblyInfo.cs
+++ src/Helpers.Net.Resource.SqlProvider/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Helpers.Net.Resource.Web")]
+[assembly: AssemblyTitle("Helpers.Net.Resource.SqlProvider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Prabir Shrestha")]
-[assembly: AssemblyProduct("Helpers.Net.Resource.Web")]
+[assembly: AssemblyProduct("Helpers.Net.Resource.SqlProvider")]
 [assembly: AssemblyCopyright("Copyright © Prabir Shrestha 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d63440a9-a902-422e-a558-5a09ea28e0bb")]
+[assembly: Guid("e6a9cd96-5f7b-46d6-a745-bb96582d3991")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
